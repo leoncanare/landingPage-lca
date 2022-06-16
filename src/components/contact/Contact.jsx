2a51769d637c2d15bdaa2da529086672113c5fef
leon.cantalapiedra <leon.cantalapiedra@bosonit.com>
2022-06-16 14:30:43 +0200

    Secciones y emailjs
    Contacto y emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
-import './contact.css'
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
+
+import "./contact.css";
+
+import { MdEmail } from "react-icons/md";
+import { FaTelegram } from "react-icons/fa";
 
 const Contact = () => {
-  return (
-    <section id='contact'>Contact</section>
-  )
-}
+  const form = useRef();
 
-export default Contact
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_jmky2j3",
+        "template_hrqhx1s",
+        form.current,
+        "bf6j6vcYuwJArDyzz"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <section id="contact">
+      <h5> Ponte en contacto </h5>
+      <h2> Conmigo </h2>
+      <div className="container contact__container">
+        <div className="contact__opotions">
+          <article className="contact__option">
+            <MdEmail className="contact__option-icon" />
+            <h4>Correo</h4>
+            <h5>leoncanare@gmail.com</h5>
+            <a href="mailto:leoncanare@gmail.com">Enviar un mensaje</a>
+          </article>
+          <article className="contact__option">
+            <FaTelegram className="contact__option-icon" />
+            <h4>Telegram</h4>
+            <h5>@leoncanare</h5>
+            <a href="mailto:leoncanare@gmail.com">Enviar un mensaje</a>
+          </article>
+          {/* <article className='contact__option'>
+          <SiLinkedin/>
+          <h4>Linkedin</h4>
+          <h5>leoncanare</h5>
+          <a href="mailto:leoncanare@gmail.com">Enviar un mensaje</a>
+        </article> */}
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Nombre completo o Nombre de la Empresa"
+            required
+          />
+          <input type="email" name="email" placeholder="Correo" required />
+          <textarea name="message" rows="10" placeholder="Mensaje"></textarea>
+          <button type="submit" className="btn btn-primary">
+            ENVIAR MENSAJE
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCards } from "swiper";
+
+import "swiper/css";
+import "swiper/css/effect-cards";
+
+const testimonialsData = [
+  {
+    avatar: AVTR1,
+    name: "Martina Mils",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat pariatur quasi obcaecati reprehenderit omnis ut, debitis repellendus qui accusamus nostrum laboriosam voluptatibus iste voluptates fugit esse quis laudantium asperiores. Officiis.",
+  },
+  {
+    avatar: AVTR2,
+    name: "Fernando Rich",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat pariatur quasi obcaecati reprehenderit omnis ut, debitis repellendus qui accusamus nostrum laboriosam voluptatibus iste voluptates fugit esse quis laudantium asperiores. Officiis.",
+  },
+  {
+    avatar: AVTR3,
+    name: "Cristian Juls",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat pariatur quasi obcaecati reprehenderit omnis ut, debitis repellendus qui accusamus nostrum laboriosam voluptatibus iste voluptates fugit esse quis laudantium asperiores. Officiis.",
+  },
+];
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section id="testimonial">
+      <h5> Opinion de mis compañeros </h5>
+      <h2> Valoraciones </h2>
+      <Swiper
+        effect={"cards"}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="container testimonials__container">
+        {testimonialsData.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt={avatar} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
 
-export default Testimonials
-
-// https://youtu.be/G-Cr00UYokU?t=8445
+export default Testimonials;
